832b532c31157aded27d2c615751b836e570b9a2
randa <randa@DESKTOP-BAR9QEM>
2025-03-26 02:41:13 +0300

    first commit

--- LearningHubCore/obj/Debug/net6.0/LearningHubCore.AssemblyInfo.cs
+++ LearningHubCore/obj/Debug/net6.0/LearningHubCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningHubCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9818a238cdf2cb3f7722e1f4a4e7e3d2883f5c2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningHubCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningHubCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
